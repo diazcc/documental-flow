bfa3047848563ce2cc1e91d1f56c581188b0cc03
Andres Diaz <diazccx@gmail.com>
2025-10-30 22:26:16 -0500

    set domain front

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 # 🔽 Configura CORS para tu aplicación.
 #    Esto le dará permiso a http://localhost:5173 para acceder a tu API.
 #    Cuando despliegues a Firebase, deberás añadir esa URL también.
-CORS(app, resources={r"/*": {"origins": ["http://localhost:5173", "https://tu-proyecto.web.app"]}})
+CORS(app, resources={r"/*": {"origins": ["http://localhost:5173", "https://portfolio-d0ea2.web.app"]}})
 
 # Leer variable de entorno FIREBASE_SERVICE_ACCOUNT
 firebase_config = os.getenv("FIREBASE_SERVICE_ACCOUNT")
