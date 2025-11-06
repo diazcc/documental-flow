ef7f859f00561cf07f5f6d2fdb31368c63f0517f
Andres Diaz <diazccx@gmail.com>
2025-11-06 13:21:55 -0500

    :bug: Detected cors

--- main.py
+++ main.py
@@ -16,12 +16,8 @@ cloudinary.config(
 )
 
 # ✅ CORS configurado para tu frontend local y desplegado
-CORS(app,
-     resources={r"/*": {"origins": ["http://localhost:5173", "https://portfolio-d0ea2.web.app"]}},
-     supports_credentials=True,
-     methods=["GET", "POST", "PATCH", "DELETE", "OPTIONS"],
-     allow_headers=["Content-Type", "Authorization"]
-)
+CORS(app, resources={r"/*": {"origins": ["http://localhost:5173", "https://portfolio-d0ea2.web.app", "https://documental-flow.onrender.com"]}}, supports_credentials=True)
+
 
 # ✅ Inicializar Firebase
 firebase_config = os.getenv("FIREBASE_SERVICE_ACCOUNT")
